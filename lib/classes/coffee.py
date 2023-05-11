f8278f26d340e12d3e41a40582cfab6f2e086979
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-05-11 11:28:40 -0400

    done

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -1,17 +1,35 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
-        
+        self._orders = []
+        self._customers = []
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if type(name) == str and not hasattr(self, "name"):
+            self._name = name
+        else:
+            raise Exception("Invalid coffee name")
+
     def orders(self, new_order=None):
         from classes.order import Order
-        pass
+        if isinstance(new_order, Order):
+            self._orders.append(new_order)
+        return self._orders
     
     def customers(self, new_customer=None):
         from classes.customer import Customer
-        pass
+        if isinstance(new_customer, Customer) and new_customer not in self._customers:
+            self._customers.append(new_customer)
+        return self._customers
     
     def num_orders(self):
-        pass
+        return len(self._orders)
     
     def average_price(self):
-        pass
+        coff_price = [c.price for c in self._orders]
+        if coff_price:
+            average = sum(coff_price)/ len(coff_price)
+        return average
--- lib/classes/order.py
+++ lib/classes/order.py
@@ -1,7 +1,49 @@
+from classes.customer import Customer
+from classes.coffee import Coffee
 
 class Order:
+
+    all =[]
 
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+
+        Order.all.append(self)
+
+        customer.coffees(self.coffee)
+        coffee.customers(self.customer)
+
+        coffee.orders(self)
+        customer.orders(self)
+    
+    @property
+    def price(self):
+        return self._price
+    @price.setter
+    def price(self, price):
+        if type(price) == int and 1 <= price <= 10:
+            self._price = price
+        else:
+            raise Exception("Invalid price")
+
+    @property
+    def customer(self):
+        return self._customer
+    @customer.setter
+    def customer(self, customer):
+        if isinstance(customer, Customer):
+            self._customer = customer
+        else:
+            raise Exception("Not a customer object")
+        
+    @property
+    def coffee(self):
+        return self._coffee
+    @coffee.setter
+    def coffee(self, coffee):
+        if isinstance(coffee, Coffee):
+            self._coffee = coffee
+        else:
+            raise Exception("Not a coffee object")
